b70ded72edb097c9efdc6d037bd2553c9f31d726
George Doujaiji <georgedouj88@gmail.com>
2020-07-20 04:37:49 -0400

    Add hexcolor command

--- cogs/color.py
+++ cogs/color.py
@@ -8,8 +8,13 @@ class Color(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def hexcolor(self, ctx, ):
-        pass
+    async def hexcolor(self, ctx, hex_code):
+        if not hex_code.isalnum():  # checks if it contains letters and numbers only
+            embed = discord.Embed(title=':X: **Error:** Hexcode must only contain letters and numbers and be exactly 6 characters long', color=0xff0000)
+            await ctx.send(embed=embed)
+            return
+        embed = discord.Embed(title=hex_code.upper(), color=int(hex_code, 16))
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
